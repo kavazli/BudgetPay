17419b9dd14ae632e1ef123d24eedcf0f564d9fd
gökhan <kavazlikaya@gmail.com>
2025-11-29 23:16:45 +0300

    init1

---
+++ BudgetPay.Domain/Employee.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Reflection.Emit;
+
+namespace BudgetPay.Domain;
+
+public class Employee
+{
+    public string FullName { get; set; } = string.Empty;
+    public string NationalIdNumber { get; set; } = string.Empty;
+
+    public string Department { get; set; } = string.Empty;
+    public string CostCenter { get; set; } = string.Empty;
+    public string PayType { get; set; } = string.Empty;
+
+    public decimal BaseSalary { get; set; }
+    public decimal SalaryIncreaseRate { get; set; }
+
+    public decimal PlannedMonthlyOvertimeHours { get; set; }
+
+    public decimal PlannedBonusAmount { get; set; }
+    public decimal BonusFrequency { get; set; }
+
+    public decimal PlannedVoucherAmount { get; set; }
+    public decimal VoucherFrequency { get; set; }
+}
+
